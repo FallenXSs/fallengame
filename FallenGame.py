3689b3d8ce082caebd820fa2876735cc9f36998c
FallenXSs <131010263+FallenXSs@users.noreply.github.com>
2023-09-05 20:55:34 +0400

    Update FallenGame.py

--- FallenGame.py
+++ FallenGame.py
@@ -1,113 +1,41 @@
 import telebot
-import random
 
-# Telegram bot tokenınızı buraya ekleyin
-TOKEN = '6514609660:AAH0hFPPgYVS4am21LQ5uRapVmbPdRJA5ns'
-
-print("Fallen Game Is Only ❌⭕")
-
-# Botunuzun instance'ını oluşturun
+TOKEN = "6514609660:AAH0hFPPgYVS4am21LQ5uRapVmbPdRJA5ns")
+"
 bot = telebot.TeleBot("6514609660:AAH0hFPPgYVS4am21LQ5uRapVmbPdRJA5ns")
+")
 
-# Etiketlenen kullanıcının hamlesini bekleyen oyun
-games = {}
+oyuncu1 = "❌"
+oyuncu2 = "⭕"
+hane_durum = ["", "", "", "", "", "", "", "", ""]  # 9 adet hane durumu
 
-# /start komutunu işleyen fonksiyon
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Hoş geldiniz! Oyunu başlatmak için /oyun komutunu kullanın. Bir sorunla karşılaşırsanız @BenYakup lütfen bildiriniz.")
+    bot.reply_to(message, "FAllen game bot'a hoşgeldiniz bu bot Yakup Karahan tarafından ❌⭕ oyunu olarak tasarlanmıştır! destek ve yardım için @BenYakup")
 
-# /oyun komutunu işleyen fonksiyon
-@bot.message_handler(commands=['oyun'])
-def start_game(message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    user_name = message.from_user.first_name
-
-    # Oyunu zaten başlatan kullanıcı varsa
-    if chat_id in games:
-        bot.send_message(chat_id, "Zaten bir oyun başlatılmış!")
-        return
-
-    # Oyunu başlatan kullanıcıya hoş geldin mesajı gönder
-    games[chat_id] = {
-        'turn': user_id,
-        'board': [[' ' for _ in range(3)] for _ in range(3)]
-    }
-    bot.send_message(chat_id, f"{user_name} oyunu başlattı! İlk hamle sizden.")
-
-# Oyun tahtasını çizmek için yardımcı fonksiyon
-def draw_board(board):
-    lines = []
-    for row in board:
-        line = ' | '.join(row)
-        lines.append(line)
-    sep = '-' * (len(line) + 2)
-    return f"\n{sep}\n".join(lines)
-
-# Oyun hamlesini işleyen fonksiyon
 @bot.message_handler(func=lambda message: True)
-def make_move(message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    user_name = message.from_user.first_name
-    text = message.text
+def hamle_yap(message):
+    # Hamle yapılacak hane sayısını alın
+    hane_sayisi = int(message.text)
 
-    if chat_id not in games:
-        return
+    # Hane numarasını (1-9 arası) hane indeksine dönüştürün
+    hane_indeks = hane_sayisi - 1
 
-    game = games[chat_id]
+    # Belirtilen hanenin durumunu kontrol edin
+    if hane_durum[hane_indeks] == '':
+        # Oyuna göre hanenin durumunu belirleyin
+        if message.from_user.id == 123456789:  # İlk oyuncunun ID'si
+            hane_durum[hane_indeks] = oyuncu1
+        elif message.from_user.id == 987654321:  # İkinci oyuncunun ID'si
+            hane_durum[hane_indeks] = oyuncu2
+        else:
+            bot.reply_to(message, "Bu oyuncu oynamaya yetkili değil!")
+            return
+        
+        # Güncellenmiş hane durumunu gönderin
+        bot.reply_to(message, f"{message.from_user.first_name} tarafından {hane_durum[hane_indeks]} hamlesi yapıldı!\nHamle sonrası durum: {hane_durum}")
+    else:
+        bot.reply_to(message, "Bu hane zaten dolu!")
 
-    # Sadece oyun sırası olan kullanıcının hamlesini işle
-    if user_id != game['turn']:
-        bot.send_message(chat_id, "Şu anda sizin sıranız değil!")
-        return
-
-    # Geçerli hamle için doğru formatı kontrol et
-    if not text.startswith('/') or len(text) != 4 or not text[1:].isdigit():
-        bot.send_message(chat_id, "Geçersiz hamle formatı!")
-        return
-
-    # Oyuncunun hamlesini al ve tahtayı güncelle
-    move = int(text[1:]) - 1
-    row = move // 3
-    col = move % 3
-
-    if game['board'][row][col] != ' ':
-        bot.send_message(chat_id, "Geçersiz hamle!")
-        return
-
-    game['board'][row][col] = '❌' if user_id == game['turn'] else '⭕️'
-    game['turn'] = random.choice([user_id for user_id in game.keys() if user_id != 'turn'])
-
-    # Oyun tahtasını gönder
-    board = draw_board(game['board'])
-    bot.send_message(chat_id, board)
-
-    # Kazananı kontrol et
-    if check_winner(game['board']):
-        bot.send_message(chat_id, f"{user_name} oyunu kazandı! Oyun bitti.")
-        del games[chat_id]
-
-    # Tahta dolu ise oyunu berabere bitir
-    elif ' ' not in [cell for row in game['board'] for cell in row]:
-        bot.send_message(chat_id, "Oyun berabere bitti. Oyun bitti.")
-        del games[chat_id]
-
-
-# Kazananı kontrol eden yardımcı fonksiyon
-def check_winner(board):
-    for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2] != ' ':
-            return True
-        if board[0][i] == board[1][i] == board[2][i] != ' ':
-            return True
-    if board[0][0] == board[1][1] == board[2][2] != ' ':
-        return True
-    if board[0][2] == board[1][1] == board[2][0] != ' ':
-        return True
-    return False
-
-
-# Botu çalıştır
 bot.polling()
+            
