df1b1dbeb9ab81f715da8dd0bf17a1d1c4916593
FallenXSs <131010263+FallenXSs@users.noreply.github.com>
2023-09-05 21:01:11 +0400

    Update FallenGame.py

--- FallenGame.py
+++ FallenGame.py
@@ -2,6 +2,8 @@ import telebot
 
 TOKEN = "6514609660:AAH0hFPPgYVS4am21LQ5uRapVmbPdRJA5ns"
 
+print("Fallen Game Is Only")
+
 bot = telebot.TeleBot("6514609660:AAH0hFPPgYVS4am21LQ5uRapVmbPdRJA5ns")
 
 oyuncu1 = "❌"
